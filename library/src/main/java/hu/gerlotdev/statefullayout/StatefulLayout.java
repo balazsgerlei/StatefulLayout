6874a878e738f6233ec32bb9dd9e94d422a943b9
Balázs Gerlei <gerlotdev@gmail.com>
2018-11-28 11:59:01 +0100

    Fixed exception on incorrect child count (moved checking to a separate method). Added setters for different views to be able to set them programatically. Implemented basic show methods. Couple of other fixes.

--- library/src/main/java/hu/gerlotdev/statefullayout/StatefulLayout.java
+++ library/src/main/java/hu/gerlotdev/statefullayout/StatefulLayout.java
@@ -3,6 +3,7 @@ package hu.gerlotdev.statefullayout;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.IntDef;
+import android.support.annotation.LayoutRes;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +17,14 @@ public class StatefulLayout extends ViewFlipper {
     @IntDef({LayoutState.EMPTY, LayoutState.LOADING, LayoutState.ERROR, LayoutState.CONTENT})
     @Retention(RetentionPolicy.SOURCE)
     public @interface LayoutState {
-        int CONTENT = 0;
-        int EMPTY = 1;
-        int LOADING = 2;
-        int ERROR = 3;
+        int EMPTY = 0;
+        int LOADING = 1;
+        int ERROR = 2;
+        int CONTENT = 3;
     }
+
+    @LayoutState
+    private int layoutState = LayoutState.CONTENT;
 
     private LayoutInflater layoutInflater;
 
@@ -31,70 +35,146 @@ public class StatefulLayout extends ViewFlipper {
 
     public StatefulLayout(Context context) {
         super(context);
+        layoutInflater = LayoutInflater.from(getContext());
     }
 
     public StatefulLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         layoutInflater = LayoutInflater.from(getContext());
-        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.StatefulLayout);
 
-        int emptyViewResId = a.getResourceId(R.styleable.StatefulLayout_sl_emptyView, -1);
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.StatefulLayout);
+
+        int emptyViewResId = typedArray.getResourceId(R.styleable.StatefulLayout_sl_emptyView, -1);
         if (emptyViewResId > -1) {
-            emptyView = layoutInflater.inflate(emptyViewResId, this, false);
-            addView(emptyView, emptyView.getLayoutParams());
+            setEmptyView(emptyViewResId);
         }
 
-        int loadingViewResId = a.getResourceId(R.styleable.StatefulLayout_sl_loadingView, -1);
+        int loadingViewResId = typedArray.getResourceId(R.styleable.StatefulLayout_sl_loadingView, -1);
         if (loadingViewResId > -1) {
-            loadingView = layoutInflater.inflate(loadingViewResId, this, false);
-            addView(loadingView, loadingView.getLayoutParams());
+            setLoadingView(loadingViewResId);
         }
 
-        int errorViewResId = a.getResourceId(R.styleable.StatefulLayout_sl_errorView, -1);
+        int errorViewResId = typedArray.getResourceId(R.styleable.StatefulLayout_sl_errorView, -1);
         if (errorViewResId > -1) {
-            errorView = layoutInflater.inflate(errorViewResId, this, false);
-            addView(errorView, errorView.getLayoutParams());
+            setErrorView(errorViewResId);
+        }
+
+        int state = typedArray.getInt(R.styleable.StatefulLayout_sl_layoutState, LayoutState.CONTENT);
+        switch (state) {
+            case LayoutState.EMPTY:
+                layoutState = LayoutState.EMPTY;
+                break;
+            case LayoutState.LOADING:
+                layoutState = LayoutState.LOADING;
+                break;
+            case LayoutState.ERROR:
+                layoutState = LayoutState.ERROR;
+                break;
+            default:
+                layoutState = LayoutState.CONTENT;
+                break;
         }
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        if (getChildCount() > 1) {
-            throw new IllegalStateException("StatefulLayout can host only one direct child, specify layout for states other than content with sl_errorView, sl_loadingView, sl_emptyView attributes");
-        } else if (getChildCount() == 1) {
-            contentView = getChildAt(LayoutState.CONTENT);
-        } else {
-            contentView = null;
+        if (!isChildCountCorrect()) {
+            throw new IllegalStateException("StatefulLayout can host exactly one direct child, specify layout for states other than content using sl_errorView, sl_loadingView, sl_emptyView xml attributes or setters");
+        }
+
+        contentView = getChildAt(LayoutState.CONTENT);
+        switch (layoutState) {
+            case LayoutState.EMPTY:
+                showEmpty();
+                break;
+            case LayoutState.LOADING:
+                showLoading();
+                break;
+            case LayoutState.ERROR:
+                showError();
+                break;
+            default:
+                showContent();
+                break;
         }
     }
 
-    public void showEmpty() {
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+    }
 
+    public void setEmptyView(@LayoutRes int emptyViewResId) {
+        emptyView = layoutInflater.inflate(emptyViewResId, this, false);
+        addView(emptyView, emptyView.getLayoutParams());
+    }
+
+    public void setLoadingView(View loadingView) {
+        this.loadingView = loadingView;
+    }
+
+    public void setLoadingView(@LayoutRes int loadingViewResId) {
+        loadingView = layoutInflater.inflate(loadingViewResId, this, false);
+        addView(loadingView, loadingView.getLayoutParams());
+    }
+
+    public void setErrorView(View errorView) {
+        this.errorView = errorView;
+    }
+
+    public void setErrorView(@LayoutRes int errorViewResId) {
+        errorView = layoutInflater.inflate(errorViewResId, this, false);
+        addView(errorView, errorView.getLayoutParams());
+    }
+
+    public void showContent() {
+        if (contentView != null && indexOfChild(contentView) != -1) {
+            setDisplayedChild(indexOfChild(contentView));
+        }
     }
 
     public void showEmpty(String message) {
 
     }
 
-    public void showContent() {
-
-    }
-
-    public void showLoading() {
-
+    public void showEmpty() {
+        if (emptyView != null && indexOfChild(emptyView) != -1) {
+            setDisplayedChild(indexOfChild(emptyView));
+        }
     }
 
     public void showLoading(String message) {
 
     }
 
-    public void showError() {
-
+    public void showLoading() {
+        if (loadingView != null && indexOfChild(loadingView) != -1) {
+            setDisplayedChild(indexOfChild(loadingView));
+        }
     }
 
     public void showError(String message) {
 
+    }
+
+    public void showError() {
+        if (errorView != null && indexOfChild(errorView) != -1) {
+            setDisplayedChild(indexOfChild(errorView));
+        }
+    }
+
+    private boolean isChildCountCorrect() {
+        int expectedChildCount = 1;
+        if (emptyView != null) {
+            expectedChildCount++;
+        }
+        if (loadingView != null) {
+            expectedChildCount++;
+        }
+        if (errorView != null) {
+            expectedChildCount++;
+        }
+        return getChildCount() == expectedChildCount;
     }
 
 }
